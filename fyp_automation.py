23418b453181fb72f4c5aa65da30ffea09654844
Hamza <hm.khanzada786@gmail.com>
2020-04-16 07:38:18 +0500

    file opened

--- fyp_automation.py
+++ fyp_automation.py
@@ -4,26 +4,52 @@ import pyautogui as pg
 # pg.rightClick('vscode_icon.png')
 # pg.click('vscode_angular-updated_tile.png')
 
-vscode_icon = None
-# i = 0
 
-while vscode_icon is None:
-    vscode_icon = pg.locateOnScreen('vscode_icon_dark.png')
-    if vscode_icon is None:
-        vscode_icon = pg.locateOnScreen('vscode_icon_light.png')
-    if vscode_icon is None:
-        vscode_icon = pg.locateOnScreen('vscode_icon_tiled.png')
-    # i += 1
-    # print(i)
+def click_vscode():
+    vscode_icon = None
+    # i = 0
+    while vscode_icon is None:
+        vscode_icon = pg.locateOnScreen('vscode_icon_dark.png')
+        if vscode_icon is None:
+            vscode_icon = pg.locateOnScreen('vscode_icon_light.png')
+        if vscode_icon is None:
+            vscode_icon = pg.locateOnScreen('vscode_icon_tiled.png')
+        if vscode_icon is None:
+            vscode_icon = pg.locateOnScreen('vscode_icon_multiple.png')
+        # i += 1
+        # print(i)
 
-# right click vscode icon
-pg.rightClick(pg.center(vscode_icon))
+    # right click vscode icon
+    pg.rightClick(pg.center(vscode_icon))
+
+
+click_vscode()
 
 vscode_angular_tile = None
-# i = 0
+# # i = 0
 
 while vscode_angular_tile is None:
     vscode_angular_tile = pg.locateOnScreen('vscode_angular-updated_tile.png')
-    print(vscode_angular_tile)
-    # i += 1
-    # print(i)
+    if vscode_angular_tile is None:
+        vscode_angular_tile = pg.locateOnScreen(
+            'vscode_angular-updated_tile_light.png')
+#     # i += 1
+#     # print(i)
+
+# click vscode angular tile
+pg.click(pg.center(vscode_angular_tile))
+
+
+click_vscode()
+vscode_connectico_tile = None
+# i = 0
+while vscode_connectico_tile is None:
+    vscode_connectico_tile = pg.locateOnScreen('vscode_connectico_tile.png')
+    if vscode_connectico_tile is None:
+        vscode_connectico_tile = pg.locateOnScreen(
+            'vscode_connectico_tile_light.png')
+# i += 1
+# print(i)
+
+# click vscode angular tile
+pg.click(pg.center(vscode_connectico_tile))
